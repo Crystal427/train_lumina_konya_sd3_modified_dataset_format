f5b0149f0b7ad7291ede24b8189e53c1a49dd84e
427 Crystal <zyr2000427@hotmail.com>
2025-11-02 16:11:40 +0800

    add torch compile fix

--- library/lumina_models.py
+++ library/lumina_models.py
@@ -23,14 +23,11 @@ import math
 import os
 from typing import List, Optional, Tuple
 from dataclasses import dataclass
-
-import torch
-from torch import Tensor
-from torch.utils.checkpoint import checkpoint
-import torch.nn as nn
-import torch.nn.functional as F
-
 from library import custom_offloading_utils
+
+disable_selective_torch_compile = (
+    os.getenv("SDSCRIPTS_SELECTIVE_TORCH_COMPILE", "0") == "0"
+)
 
 try:
     from flash_attn import flash_attn_varlen_func
@@ -553,7 +550,7 @@ class JointAttention(nn.Module):
             raise RuntimeError(
                 f"Could not load flash attention. Please install flash_attn. / フラッシュアテンションを読み込めませんでした。flash_attn をインストールしてください。 / {e}"
             )
-
+@torch.compiler.disable(reason="complex ops inside")
 def apply_rope(
     x_in: torch.Tensor,
     freqs_cis: torch.Tensor,
@@ -630,11 +627,9 @@ class FeedForward(nn.Module):
         )
         nn.init.xavier_uniform_(self.w3.weight)
 
-    def _forward_silu_gating(self, x1, x3):
-        return F.silu(x1) * x3
-
+    @torch.compile(disable=disable_selective_torch_compile)
     def forward(self, x):
-        return self.w2(self._forward_silu_gating(self.w1(x), self.w3(x)))
+        return self.w2(F.silu(self.w1(x))*self.w3(x))
 
 
 class JointTransformerBlock(GradientCheckpointMixin):
@@ -700,6 +695,7 @@ class JointTransformerBlock(GradientCheckpointMixin):
             nn.init.zeros_(self.adaLN_modulation[1].weight)
             nn.init.zeros_(self.adaLN_modulation[1].bias)
 
+    @torch.compile(disable=disable_selective_torch_compile)
     def _forward(
         self,
         x: torch.Tensor,
@@ -790,7 +786,7 @@ class FinalLayer(GradientCheckpointMixin):
         )
         nn.init.zeros_(self.adaLN_modulation[1].weight)
         nn.init.zeros_(self.adaLN_modulation[1].bias)
-
+    @torch.compile(disable=disable_selective_torch_compile)
     def forward(self, x, c):
         scale = self.adaLN_modulation(c)
         x = modulate(self.norm_final(x), scale)
@@ -1224,6 +1220,7 @@ class NextDiT(nn.Module):
         return output
 
     @staticmethod
+    @torch.compiler.disable(reason="complex ops inside")
     def precompute_freqs_cis(
         dim: List[int],
         end: List[int],
